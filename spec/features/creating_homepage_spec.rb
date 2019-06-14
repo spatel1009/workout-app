f1538d01f83f13c5fb60885f2272dddbb310e698
spatel1009 <spatel1009@gmail.com>
2019-06-14 16:44:17 -0400

    added nav bar

--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -4,7 +4,6 @@ RSpec.feature 'Creating Homepage' do
   scenario 'user successfully visits homepage' do
     visit '/'
 
-    expect(page).to have_link('Home')
     expect(page).to have_link('Athletes Den')
     expect(page).to have_content('Workout Lounge!')
     expect(page).to have_content('Show off your workout')
