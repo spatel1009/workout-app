787b5a2451bd6effcc484aa2a30ad58301f1e89a
spatel1009 <spatel1009@gmail.com>
2019-06-21 15:20:32 -0400

    implemented following friends feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_225931) do
+ActiveRecord::Schema.define(version: 2019_06_21_184353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2019_06_17_225931) do
     t.datetime "updated_at", null: false
     t.integer "count", default: 0
     t.index ["user_id"], name: "index_exercises_on_user_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +50,5 @@ ActiveRecord::Schema.define(version: 2019_06_17_225931) do
   end
 
   add_foreign_key "exercises", "users"
+  add_foreign_key "friendships", "users"
 end
