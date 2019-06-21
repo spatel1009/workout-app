49cc2828ef3ffdbda713898d809724f74e3b6b34
spatel1009 <spatel1009@gmail.com>
2019-06-21 14:36:13 -0400

    implemented test for following users

---
+++ spec/features/following_users/following_friends_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature 'Searching for Users' do
+  before do
+    @sohel = User.create!(email: 'sohel@test.com', first_name: 'sohel', last_name: 'anon', password: '123456')
+    @john = User.create!(email: 'john@test.com', first_name: 'john', last_name: 'daby', password: '123456')
+    login_as(@sohel)
+  end
+
+  scenario 'if signed in' do
+    visit '/'
+
+    expect(page).to have_content(@john.full_name)
+    expect(page).to have_content(@sohel.full_name)
+
+    href = "/friendships?friend_id=#{@sohel.id}"
+    expect(page).not_to have_link("Follow", href: href)
+
+    link = "a[href='/friendships?friend_id=#{@john.id}']"
+    find(link).click
+
+    href = "/friendships?friend_id=#{@john.id}"
+    expect(page).not_to have_link("Follow", href: href)
+  end
+end
